b7f75fff813e7e77b19a0f66bfebc83f1a6cf8b8
Dyme <hugosimpson275@gmail.com>
2025-01-17 09:53:16 +0000

    init commit

---
+++ Exploding_Coin/PluginCore.cs
@@ -0,0 +1,31 @@
+﻿using Exiled.API.Features;
+
+namespace Exploding_Coin {
+    public class PluginCore : Plugin<Config> {
+        public override void OnEnabled() {
+            base.OnEnabled();
+            BindEvents();
+            RegisterItems();
+        }
+
+        public override void OnDisabled() {
+            base.OnDisabled();
+            UnbindEvents();
+            UnregisterItems();
+        }
+        void BindEvents() {
+            
+        }
+
+        void UnbindEvents() {
+            
+        }
+        void RegisterItems() {
+            
+        }
+
+        void UnregisterItems() {
+            
+        }
+    }
+}
